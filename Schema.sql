21964c019ad87b7d37745bab4cfb251f87fb0973
pmagnuson17 <phillipmagnuson17@mail.fresnostate.edu>
2021-05-01 21:44:09 -0700

    Initial commit

---
+++ Schema.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA `googleplay` ;
+
+create table Applications (App mediumtext, Category tinytext, Rating float(2,1), Reviews bigint, Size tinytext, Installs bigint, Paid bool, Price float(5,2), Content_Rating mediumtext, Last_Updated mediumtext, Current_Version mediumtext, Android_Version mediumtext);
+create table Reviews (App mediumtext, Review longtext, Sentiment mediumtext, Sentiment_Polarity float(6,5), Sentiment_Subjectivity float(7,6), Person_ID int);
+create table People (id bigint, Gender text, Age int);
